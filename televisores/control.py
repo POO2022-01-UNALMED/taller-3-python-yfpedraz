4830bc9af3639701d8c94d5eea3bae8ffb7f4dae
Estebanmartinac1190 <92158255+Estebanmartinac1190@users.noreply.github.com>
2022-03-29 23:31:01 -0500

    :+1:

---
+++ televisores/control.py
@@ -0,0 +1,36 @@
+class Control:
+
+    def __init__(self):
+        from televisores.tv import TV
+        self._tv = None
+    
+    def enlazar (self, televisor):
+        self._tv = televisor
+        televisor.setControl(self)
+
+    def turnOn(self):
+        self._tv.turnOn()
+
+    def turnOff(self):
+        self._tv.turnOff()
+    
+    def canalUp(self):
+        self._tv.canalUp()
+    
+    def canalDown(self):
+        self._tv.canalDown()
+    
+    def volumenUp(self):
+        self._tv.volumenUp()
+    
+    def volumenDown(self):
+        self._tv.volumenDown()
+
+    def setCanal(self, nuevoCanal):
+        self._tv.setCanal(nuevoCanal)
+    
+    def getTv(self):
+        return self._tv
+    
+    def setTv(self, televisor):
+        self.tv = televisor
